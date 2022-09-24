9fa212e0740ffb44a237482ece28b3c3f61e51a9
emonibnsalim <emonibnsali@gmail.com>
2022-09-24 15:44:58 +0600

    practise-simple

---
+++ src/pages/About.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            dee
+        </div>
+    );
+};
+
+export default About;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            hwello
+        </div>
+    );
+};
+
+export default Home;
